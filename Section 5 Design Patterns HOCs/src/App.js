a41317ea519f2548ec36a70e7febc1294c6fc1eb
unknown <philip.ewane97@gmail.com>
2024-01-18 10:50:08 +0100

    Loading data using HOC

--- Section 5 Design Patterns HOCs/src/App.js
+++ Section 5 Design Patterns HOCs/src/App.js
@@ -1,12 +1,16 @@
 import { checkProps } from "./components/check-props";
+import { includeUser } from "./components/include-user";
 import { UserInfo } from "./components/user-info";
 
 const UserInfoWrapper = checkProps(UserInfo);
 
+const UserInfoWithLoader = includeUser(UserInfo, "3");
+
 function App() {
   return (
     <>
-      <UserInfoWrapper propA="test1" blabla={{ a: 1, age: 23 }} />
+      {/* <UserInfoWrapper propA="test1" blabla={{ a: 1, age: 23 }} /> */}
+      <UserInfoWithLoader />
     </>
   );
 }
