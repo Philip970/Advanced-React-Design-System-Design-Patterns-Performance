b188a8aaab459e3e0be7799fa923a1bf4bd71137
unknown <philip.ewane97@gmail.com>
2024-03-12 11:36:03 +0100

    Optimizing context API

--- section 10 Clean Code Tips/src/App.jsx
+++ section 10 Clean Code Tips/src/App.jsx
@@ -1,5 +1,10 @@
 import Button from "./components/button";
 
+import Buttons from "./components/buttons";
+import { CartProvider } from "./components/cart-context";
+import Display from "./components/display";
+import "./style.css";
+
 const style = {
   display: "flex",
   flexDirection: "column",
@@ -9,7 +14,10 @@ const style = {
 
 function App() {
   return (
-    <div style={style}>
+    <CartProvider>
+      <Display />
+      <Buttons />
+      {/* <div style={style}>
       <Button size="s">Small</Button>
       <Button size="m">Medium</Button>
       <Button size="l">Large</Button>
@@ -17,7 +25,8 @@ function App() {
       <Button As="a" href="/">
         Link
       </Button>
-    </div>
+    </div> */}
+    </CartProvider>
   );
 }
 
